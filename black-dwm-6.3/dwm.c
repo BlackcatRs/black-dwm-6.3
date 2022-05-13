745bd819cbb14436c793ae44035c4212607c2ac1
vts <vithurshan@laposte.net>
2022-05-13 22:50:28 +0200

    Display color if some windows/client present on the tag

--- black-dwm-6.3/dwm.c
+++ black-dwm-6.3/dwm.c
@@ -820,24 +820,25 @@ drawbar(Monitor *m)
 		tw = m->ww - drawstatusbar(m, bh, stext);
 	}
 
+	/* for ( init; condition; increment ) */
 	for (c = m->clients; c; c = c->next) {
 		occ |= c->tags;
 		if (c->isurgent)
 			urg |= c->tags;
 	}
+
 	x = 0;
 	for (i = 0; i < LENGTH(tags); i++) {
 		w = TEXTW(tags[i]);
-		drw_setscheme(drw, (m->tagset[m->seltags] & 1 << i ? tagscheme[i] : scheme[SchemeNorm]));
+		drw_setscheme(drw, (occ & 1 << i ? tagscheme[i] : scheme[SchemeNorm] ));
 		drw_text(drw, x, 0, w, bh, lrpad / 2, tags[i], urg & 1 << i);
+
+
 		if (ulineall || m->tagset[m->seltags] & 1 << i) /* if there are conflicts, just move these lines directly underneath both 'drw_setscheme' and 'drw_text' :) */
 			drw_rect(drw, x + ulinepad, bh - ulinestroke - ulinevoffset, w - (ulinepad * 2), ulinestroke, 1, 0);
-		/* if (occ & 1 << i) */
-		/* 	drw_rect(drw, x + boxs, boxs, boxw, boxw, */
-		/* 		m == selmon && selmon->sel && selmon->sel->tags & 1 << i, */
-		/* 		urg & 1 << i); */
 		x += w;
 	}
+
 	w = blw = TEXTW(m->ltsymbol);
 	drw_setscheme(drw, scheme[SchemeNorm]);
 	x = drw_text(drw, x, 0, w, bh, lrpad / 2, m->ltsymbol, 0);
