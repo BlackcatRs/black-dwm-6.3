252f4496866f512407d3ac92d50c5e2337613c29
vts <vithurshan@laposte.net>
2022-07-08 19:44:05 +0200

    Change the font of dwm tags and dwm bar

--- black-dwm-6.3/config.def.h
+++ black-dwm-6.3/config.def.h
@@ -11,7 +11,7 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 /* static const char *fonts[]          = { "monospace:size=10" }; */
-static const char *fonts[]          = { "JetBrainsMono Nerd Font:style:medium:size=11",
+static const char *fonts[]          = { "FuraCode Nerd Font:style:Regular:size=11",
                                         "Material Design Icons-Regular:size=11" };
 
 static const char dmenufont[]       = "JetBrainsMono Nerd Font:style:medium:size=11";
